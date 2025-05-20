2513b9c0e70020c559474e9eb32708ccfe745752
Pangjing-Wu <pangjing.wu@outlook.com>
2025-05-20 16:30:14 +0800

    fix a bug in installing ssh

--- init/install_miniconda.sh
+++ init/install_miniconda.sh
@@ -1,3 +1,8 @@
+# Install and start the SSH server
+sudo apt update
+sudo apt install -y openssh-server
+sudo service ssh start
+
 # Check if Miniconda is already installed
 if [ -d "$HOME/miniconda3" ] && [ -x "$HOME/miniconda3/bin/conda" ]; then
     echo "✅ Miniconda is already installed at $HOME/miniconda3. Skipping installation."
@@ -8,15 +13,6 @@ mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
-
-
-# Switch to the newly created user
-
-
-# Install and start the SSH server
-sudo apt update
-sudo apt install -y openssh-server
-sudo service ssh start
 
 ~/miniconda3/bin/conda init zsh
 source ~/.zshrc
